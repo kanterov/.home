2a1d118a35d46fb5554d339534b749b9facb57cb
Susan Potter <me@susanpotter.net>
2013-12-01 15:57:04 -0600

    Fix setup.sh logic for 'rerun' case

--- setup.sh
+++ setup.sh
@@ -5,13 +5,29 @@ SF_DIR=${PWD}/bin
 DOTFILES=$(ls ${DF_DIR})
 SCRIPTS=$(ls ${SF_DIR})
 
+# Takes three arguments:
+# 1. src path
+# 2. dst path
+function linkDotfile() {
+  local src=$1;
+  local dst=$2;
+
+  if [ ! -e ${dst} ]; then
+    ln -s ${src} ${dst}
+  elif [ "$(readlink ${dst})" == ${src} ]; then
+    echo "info: already linked ${src}";
+  else
+    echo "warning: ${dst} already exists";
+  fi;
+}
+
 for df in ${DOTFILES}; {
   bf=$(basename ${df});
-  ln -Fs ${DF_DIR}/${bf} ${HOME}/.${bf};
+  linkDotfile "${DF_DIR}/${bf}" "${HOME}/.${bf}"
 }
 
 mkdir -p ${HOME}/bin;
 for sf in ${SCRIPTS}; {
   sf=$(basename ${sf});
-  ln -Fs ${SF_DIR}/${sf} ${HOME}/bin/${sf};
+  linkDotfile "${SF_DIR}/${sf}" "${HOME}/bin/${sf}"
 }
