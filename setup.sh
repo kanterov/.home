78c4fe7d1cfee204e982fbec5468ff7e2b47902c
Susan Potter <me@susanpotter.net>
2015-07-01 14:26:12 -0500

    Fix setup lint problems

--- setup.sh
+++ setup.sh
@@ -1,33 +1,45 @@
 #!/usr/bin/env bash
 
-DF_DIR=${PWD}/dotfiles
-SF_DIR=${PWD}/bin
-DOTFILES=$(ls ${DF_DIR})
-SCRIPTS=$(ls ${SF_DIR})
+declare -r df_dir="${PWD}/dotfiles"
+declare -r sf_dir="${PWD}/bin"
+declare -ra dotfiles=($(ls "${df_dir}"))
+declare -ra scripts=($(ls "${sf_dir}"))
 
 # Takes three arguments:
 # 1. src path
 # 2. dst path
 function linkDotfile() {
-  local src=$1;
-  local dst=$2;
+  local src="$1";
+  local dst="$2";
 
-  if [ ! -e ${dst} ]; then
-    ln -s ${src} ${dst}
-  elif [ "$(readlink ${dst})" == ${src} ]; then
+  if [ ! -e "${dst}" ]; then
+    ln -s "${src}" "${dst}"
+  elif [ "$(readlink "${dst}")" = "${src}" ]; then
     echo "info: already linked ${src}";
   else
     echo "warning: ${dst} already exists";
   fi;
 }
 
-for df in ${DOTFILES}; {
-  bf=$(basename ${df});
-  linkDotfile "${DF_DIR}/${bf}" "${HOME}/.${bf}"
+function linkDotfiles() {
+  local bf
+
+  for df in "${dotfiles[@]}"; {
+    bf="$(basename ${df})";
+    linkDotfile "${df_dir}/${bf}" "${HOME}/.${bf}"
+  }
 }
 
-mkdir -p ${HOME}/bin;
-for sf in ${SCRIPTS}; {
-  sf=$(basename ${sf});
-  linkDotfile "${SF_DIR}/${sf}" "${HOME}/bin/${sf}"
+function linkBinScripts() {
+  local sf
+
+  mkdir -p "${HOME}/bin";
+  for sf in "${scripts[@]}"; {
+    sf="$(basename "${sf}")";
+    linkDotfile "${sf_dir}/${sf}" "${HOME}/bin/${sf}"
+  }
 }
+
+set -eu
+linkDotfiles
+linkBinScripts
